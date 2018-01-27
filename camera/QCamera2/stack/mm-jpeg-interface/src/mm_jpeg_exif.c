5b341d766ddb0c489768c3e2ee2c1d881e8063a5
moto999999 <juan1999l@gmail.com>
2018-01-27 23:51:39 +0100

    update

--- camera/QCamera2/stack/mm-jpeg-interface/src/mm_jpeg_exif.c
+++ camera/QCamera2/stack/mm-jpeg-interface/src/mm_jpeg_exif.c
@@ -326,14 +326,14 @@ int process_sensor_data(cam_sensor_params_t *p_sensor_params,
   }
 
   /*Flash*/
-  short val_short;
+  short val_short = 0;
   int flash_mode_exif, flash_fired;
   if (p_sensor_params->flash_state == CAM_FLASH_STATE_FIRED) {
     flash_fired = 1;
   } else {
     flash_fired = 0;
   }
-  LOGD("Flash value %d flash mode %d flash state %d", val_short,
+  LOGD("Flash mode %d flash state %d",
     p_sensor_params->flash_mode, p_sensor_params->flash_state);
 
   switch(p_sensor_params->flash_mode) {
